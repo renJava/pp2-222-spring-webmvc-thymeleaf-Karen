9e026712f331c5f0598722755f671d772c94ed78
renJava <rendg68@gmail.com>
2023-06-16 13:07:25 +0300

    Ошибка: вместо полей реализации CarServiceImpl в классе CarController исправил на поля интерфейса CarService.

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -4,14 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.service.CarServiceImpl;
+import web.service.CarService;
 
 @Controller
 public class CarController {
-    private final CarServiceImpl carService;
+    private final CarService carService;
 
-    public CarController(CarServiceImpl carImpl) {
-        this.carService = carImpl;
+    public CarController(CarService carService) {
+        this.carService = carService;
     }
 
     @GetMapping(value = "/cars")
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -9,13 +9,13 @@ import java.util.List;
 @Service
 public class CarServiceImpl implements CarService {
     private final List<Car> car;
-
+    String rus = "Россия";
     {
         car = new ArrayList<>();
-        car.add(new Car("Нива", 2011, "Россия"));
+        car.add(new Car("Нива", 2011, rus));
         car.add(new Car("Maybach", 2013, "Германия"));
-        car.add(new Car("Aurus Senat", 2020, "Россия"));
-        car.add(new Car("УАЗ Patriot", 1993, "Россия"));
+        car.add(new Car("Aurus Senat", 2020, rus));
+        car.add(new Car("УАЗ Patriot", 1993, rus));
         car.add(new Car("Mitsubishi Outlander", 2008, "Япония"));
     }
 
@@ -26,7 +26,7 @@ public class CarServiceImpl implements CarService {
 
     @Override
     public List<Car> getCarsByCount(int count) {
-        if (count == 0 || count > 5) return car;
+        if (count <= 0 || count > 5) return car;
         return car.stream().limit(count).toList();
     }
 
